3411039d9286d6f088a7b4a3d0be909cd56592bd
m-zohaib-khan <168336266+m-zohaib-khan@users.noreply.github.com>
2024-05-29 23:31:18 +0500

    Add files via upload

---
+++ 11 program.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a=4,b=6,c;
+	
+	c=a+b;
+	printf("a+b=%d\n",c);
+	c=a-b;
+	printf("a-b=%d\n",c);
+	c=a*b;
+	printf("a-b=%d\n",c);
+	c=a/b;
+	printf("a/b=%d\n",c);
+	c=a%b;	
+	printf("remainder when a divided by b=%d\n",c);	
+	
+	return 0;
+}
